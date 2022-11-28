37114a62dd6be1d594ee904ce0c1900013cd0770
Sherif Ackles <49988463+nabesteron@users.noreply.github.com>
2022-11-28 19:45:17 +0000

    finish register page styling

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,21 +1,32 @@
 import Form from "react-bootstrap/Form";
 import "./register.css";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
 function FormGroupExample() {
   return (
     <Form>
-      <main className="container main-container m-4 p-5 w-50 mx-auto bg-dark br-2rem">
+      <main className="container main-container  p-5 w-50  bg-dark br-2rem">
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control className="email" type="email" placeholder="Enter email"  />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control className="email" type="password" placeholder="Password"  />
         </Form.Group>
+        
+        <Form.Label>Profile picture</Form.Label>
+        
         <div>
-        <Button variant="light" size="md">Sign in</Button> 
+        <Form.Control className="" type="file" variant="light" size="md" />
+            
+          
+        </div>
+        
+        <div>
+          <Button className="mt-4" variant="light" size="md" >
+            Sign in
+          </Button>
         </div>
       </main>
     </Form>
