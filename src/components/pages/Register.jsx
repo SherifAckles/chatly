1ca6e7b985997797065a5941608d5e0f48e2c68c
Sherif Ackles <49988463+nabesteron@users.noreply.github.com>
2022-11-28 18:46:20 +0000

    import and add sign in button

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,17 +1,22 @@
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import "./register.css";
+import { Button } from 'react-bootstrap';
 
 function FormGroupExample() {
   return (
     <Form>
-        <main className='container main-container'>
-      <Form.Group className="mb-3" controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+      <main className="container main-container m-4 p-5 w-50 mx-auto bg-dark br-2rem">
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <div>
+        <Button variant="light" size="md">Sign in</Button> 
+        </div>
       </main>
     </Form>
   );
