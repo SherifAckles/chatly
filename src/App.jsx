253dc9cda2b4947a3c7a18588f7fbaad76c2a4ca
Sherif Ackles <49988463+nabesteron@users.noreply.github.com>
2022-11-28 11:51:02 +0000

    use react 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Nav from "./components/nav/Nav";
-import {auth} from "./firebase"
-import {useAuthState} from " react-firebase-hooks/auth"
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const App = () => {
+  const [user] = useAuthState(auth);
+  console.log(user);
   return (
     <div>
       <Nav />
--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 
+const container = document.getElementById('root');
+// Create a root.
+const root = ReactDOM.createRoot(container);
+// Initial render
+root.render(<App name="chatly" />);
 
-ReactDOM.render(<App />, document.querySelector("#root"));
