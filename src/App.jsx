a84c526bc757cf31b014eae17847a6d3ae308f73
Sherif Ackles <49988463+nabesteron@users.noreply.github.com>
2022-11-28 15:25:16 +0000

    import register in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Register from "./components/pages/Register";
 
 import Nav from "./components/nav/Nav";
 import { auth } from "./firebase";
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <div>
       <Nav />
+      <Register />
     </div>
   );
 };
