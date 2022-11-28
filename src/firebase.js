3290a8668c835e8446dec6c4e6e767acf5328a96
Sherif Ackles <49988463+nabesteron@users.noreply.github.com>
2022-11-28 11:18:11 +0000

    creat firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCa33Ihjd7S9HTVcze6qSdhddPddA2IiZI",
+  authDomain: "chatly-bd612.firebaseapp.com",
+  projectId: "chatly-bd612",
+  storageBucket: "chatly-bd612.appspot.com",
+  messagingSenderId: "369533038593",
+  appId: "1:369533038593:web:b85028df7fd04ba5389000"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
