4343cb66edfd50e114b4954a5c46df22829ed75c
Sherif Ackles <49988463+nabesteron@users.noreply.github.com>
2022-11-29 19:29:00 +0000

    import sidebar and chat compos to home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Sidebar from "../../components/chatbox/Sidebar"
+import Chat from "../../components/chatbox/Chat"
+
 
 const Home = () => {
   return (
     <div className='home'>
       <div className="container">
-        
+        <Sidebar />
+        <Chat/>
       </div>
     </div>
   )
