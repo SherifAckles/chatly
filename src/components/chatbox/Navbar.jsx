91b0331da08a42fbce8eddfa1329884c56f45209
Sherif Ackles <49988463+nabesteron@users.noreply.github.com>
2022-11-29 21:34:40 +0000

    styling chatbox navbar

--- src/components/chatbox/Navbar.jsx
+++ src/components/chatbox/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <span className='logo-navbar'>Chatly</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>Sheriff</span>
+        <button>logout</button>
+      </div>
+
+    </div>
   )
 }
 
