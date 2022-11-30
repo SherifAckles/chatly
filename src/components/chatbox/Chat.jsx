5290c35c2e88d9fa9d5b4bc113e20fafeb7bf121
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-30 13:40:39 -0500

    creat msg comp,working on the chat.jsx

--- src/components/chatbox/Chat.jsx
+++ src/components/chatbox/Chat.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Messages from "./Messages";
 import "./chat.css";
 
 const Chat = () => {
-  return <div className="chat">chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatinfo">
+        {/* username */}
+        <span>sara</span>
+      </div>
+      <Messages />
+    </div>
+  );
 };
 
 export default Chat;
