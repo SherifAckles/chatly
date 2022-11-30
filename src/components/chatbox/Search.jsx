04d783e860d8b9f0380ea5701f4b55b98f4a1cd1
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-30 10:20:25 -0500

    work on searchbar

--- src/components/chatbox/Search.jsx
+++ src/components/chatbox/Search.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./search.css"
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div>
+      <div class="input-group flex-nowrap">
+  
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>
+</div>
+    </div>
   )
 }
 
