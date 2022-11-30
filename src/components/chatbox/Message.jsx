a758b0be89cbcc4b8fc9eb053992bb51b9e8f4e7
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-30 17:59:13 -0500

    working on msgs

--- src/components/chatbox/Message.jsx
+++ src/components/chatbox/Message.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import User1 from "../../assets/pngegg (1).png"
+import "./message.css";
 
 const message = () => {
   return (
-    <div className='message'>Message</div>
+    <div className='message'>
+      <div className="message-info">
+        <img src={User1} alt="" />
+        <span>just now</span>
+      </div>
+        <div className="messageContent">
+          <p>hello</p>
+          {/* <img src={User1} alt="" /> */}
+        </div>
+    </div>
   )
 }
 
