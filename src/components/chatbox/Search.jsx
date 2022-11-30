f6393e5edfd596bb72116487ef9288714964dee6
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-30 10:31:02 -0500

    add img to the userchat

--- src/components/chatbox/Search.jsx
+++ src/components/chatbox/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import User2 from "../../assets/pngegg (2).png"
 import "./search.css"
 
 const Search = () => {
@@ -7,6 +8,12 @@ const Search = () => {
       <div class="input-group flex-nowrap">
   
   <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>
+</div>
+      <div className="userchat">
+        <img src={User2} alt="user2" />
+        <div className="userchat-info">
+          <span>sara</span>
+        </div>
 </div>
     </div>
   )
