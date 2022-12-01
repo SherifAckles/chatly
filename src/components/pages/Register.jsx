ca9c01987a5d5fd436b64c9fc94a5785ea63fed7
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-12-01 11:57:34 -0500

    create reg handlesubmit function to handle inputs

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -4,53 +4,58 @@ import "./register.css";
 import Avatar from "../../assets/avatar.png";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-.then((userCredential) => {
-  // Signed in 
-  const user = userCredential.user;
-  // ...
-})
-.catch((error) => {
-  const errorCode = error.code;
-  const errorMessage = error.message;
-  // ..
-});
+// const auth = getAuth();
+// // createUserWithEmailAndPassword(auth, email, password)
+// .then((userCredential) => {
+//   // Signed in
+//   const user = userCredential.user;
+//   // ...
+// })
+// .catch((error) => {
+//   const errorCode = error.code;
+//   const errorMessage = error.message;
+//   // ..
+// });
 function Register() {
-  
-  
-return (
-  <main className="main-container">
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    const file = e.target[2].files[0];
+    // test working console.log(e.target[0].value, e.target[1].value, e.target[2].value)
+  };
+  return (
+    <main className="main-container">
       <span className="logo d-flex justify-content-center">
         Chatl <span className="y">y</span>
       </span>
-      <Form >
+      <Form onSubmit={handleSubmit}>
         <span className="title d-flex justify-content-center m-2">
           Register
         </span>
-        <Form.Group 
+        <Form.Group
           className="mb-3 container-form-group"
           controlId="formGroupEmail"
-          >
+        >
           <Form.Label>Email address</Form.Label>
           <Form.Control
             className="email"
             type="email"
             placeholder="Enter email"
             required
-            />
+          />
         </Form.Group>
         <Form.Group
           className="mb-3 container-form-group"
           controlId="formGroupPassword"
-          >
+        >
           <Form.Label>Password</Form.Label>
           <Form.Control
             className="email"
             type="password"
             placeholder="Password"
             required
-            />
+          />
         </Form.Group>
 
         <Form.Label>Add a profile picture</Form.Label>
@@ -62,7 +67,7 @@ return (
             id="file"
             variant="light"
             size="md"
-            />
+          />
 
           <label htmlFor="file">
             <img src={Avatar} className="avatar " alt="avatar pic" />
@@ -70,7 +75,7 @@ return (
         </div>
 
         <div>
-          <Button className="d-grid mt-2 col-6 mx-auto" size="md">
+          <Button type="submit" className="d-grid mt-2 col-6 mx-auto" size="md">
             Sign up
           </Button>
         </div>
