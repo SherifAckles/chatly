d9a6840c3991dcc47aafaf24fce3134aba7f4297
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-12-02 20:08:37 -0500

    creat funcs

--- src/components/chatbox/Chats.jsx
+++ src/components/chatbox/Chats.jsx
@@ -4,14 +4,15 @@ import "./chats.css";
 import { doc, onSnapshot } from "firebase/firestore";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
-import { db } from "firebase";
+import { db } from "../../firebase";
+
+
 
 const Chats = () => {
-
   const [chats, setChats] = useState([]);
+
   const { currentUser } = useContext(AuthContext);
   const { dispatch } = useContext(ChatContext);
-
 
   useEffect(() => {
     const getChats = () => {
@@ -34,20 +35,19 @@ const Chats = () => {
   return (
     <div className="chats">
       {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
-        <div className="userchat">
-           key={chat[0]}
+        <div
+          className="userChat"
+          key={chat[0]}
           onClick={() => handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt="" />
-        <div className="userchat-info">
-        <span>{chat[1].userInfo.displayName}</span>
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
             <p>{chat[1].lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
       ))}
-     
-
     </div>
-    
   );
 };
 
--- src/components/chatbox/Writemessage.jsx
+++ src/components/chatbox/Writemessage.jsx
@@ -1,10 +1,83 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
 import Attach from "../../assets/attach.png";
 import Image from "../../assets/image.png";
-import "./writemessage.css"
+import "./writemessage.css";
+import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore/lite";
+import { db, storage } from "../../firebase";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+
 const Writemessage = () => {
+
+const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (img) {
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        (error) => {
+          //TODO:Handle Error
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    setText("");
+    setImg(null);
+  };
+
   return (
-    <div className='writemessage'>
+    <div className="writemessage">
       <input type="text" placeholder="Send a message..." />
       <div className="send">
         <img src={Attach} alt="" />
@@ -12,10 +85,10 @@ const Writemessage = () => {
         <label htmlFor="file">
           <img src={Image} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Writemessage
+export default Writemessage;
